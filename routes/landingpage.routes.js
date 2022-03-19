35996ef45c284947bc5ed838fee57a8b65657cdf
MSadiq <mhdsadd3@gmail.com>
2022-03-19 14:21:43 +0100

    landingPage

--- routes/landingpage.routes.js
+++ routes/landingpage.routes.js
@@ -1,12 +1,11 @@
-const router = require("express").Router()
-
+const router = require("express").Router();
+const path = require("path");
 
 // @route GET /
 // @description landing page route
 // @access public
 router.get("/", (req, res) => {
-	res.sendFile("../landingPage.html");
+	res.sendFile(path.join(__dirname + "../landingPage.html"));
 });
 
 module.exports = router;
- 
