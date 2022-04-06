15aa1947bb5e08bdf5c4c8e1b776a3d858c25b53
MSadiq <mhdsadd3@gmail.com>
2022-04-06 13:18:23 +0100

    request_limit_increase

--- app.js
+++ app.js
@@ -5,21 +5,26 @@ const bodyParser = require("body-parser");
 const routers = require("./routes/index.routes");
 const connectDB = require("./configs/db");
 const app = express();
-const cors = require('cors')
-
+const cors = require("cors");
 
 // DB connect
 connectDB();
 
 // cors init
-app.use(cors())
+app.use(cors());
 
 // morgan init
 app.use(morgan("dev"));
 
 // body-parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+	bodyParser.urlencoded({
+		limit: "50mb",
+		extended: true,
+		parameterLimit: 50000,
+	})
+);
 
 // routes
 routers(app);
